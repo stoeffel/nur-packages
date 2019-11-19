55dd3306fa19932484f18b93d81a8a027d011a22
Stoeffel <schtoeffel@gmail.com>
2019-11-19 09:21:41 +0100

    add lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,10 @@
 { pkgs }:
-
-with pkgs.lib; {
-  # Add your library functions here
-  #
-  # hexint = x: hexvals.${toLower x};
-}
-
+with builtins;
+with pkgs.lib;
+let
+  readFileLines = compose [ (splitString "\n") readFile ];
+  readJSON = compose [ fromJSON readFile ];
+  compose = fs: pipe (reverseList fs);
+  pipe = fs: x: if fs != [ ] then pipe (tail fs) ((head fs) x) else x;
+  unlines = concatStringsSep "\n";
+in { inherit readFileLines readJSON compose pipe unlines; }
